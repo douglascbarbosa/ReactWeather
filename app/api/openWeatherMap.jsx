3e036989838fb6547d52158f4a9b4dc310e82ba5
Douglas Carlos <douglas.carlos@rh3software.local>
2017-02-06 18:03:22 -0300

    Inital commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=ceb9fb963d7186b30f7a011041fba178&units=metric';
+
+// ceb9fb963d7186b30f7a011041fba178
+
+module.exports = {
+	getTemp : function(location){
+		var encodedLocation = encodeURIComponent(location);
+		var requestUrl = `${OPEN_WEATHER_URL}&q=${encodedLocation}`;
+
+		return axios.get(requestUrl).then(function(res){
+			if(res.data.cod && res.data.message){
+				throw new Error(res.data.message);
+			}else{
+				return res.data.main.temp;
+			}
+		}, function(res){
+			throw new Error(res.data.message);
+		});
+	}
+}
