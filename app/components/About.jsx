0aa3618bcb8a40c24e75122b8efeca446939ebd6
Douglas Carlos <douglas.carlos@rh3software.local>
2017-02-07 11:52:09 -0300

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,16 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page.</p>
+			<h1 className="text-center">About</h1>
+			<p>This is a weather application build on React. I have Built this for The Complete React Web app Developer Course.</p>
+			<p>
+				Here are some of the tools I used:
+			</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react"> React </a>
+				</li>
+			</ul>
 		</div>
 	);
 };
--- public/bundle.js
+++ public/bundle.js
@@ -26699,23 +26699,41 @@
 /* 254 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var About = function About(props) {
 		return React.createElement(
-			'div',
+			"div",
 			null,
 			React.createElement(
-				'h3',
-				null,
-				'About'
+				"h1",
+				{ className: "text-center" },
+				"About"
 			),
 			React.createElement(
-				'p',
+				"p",
 				null,
-				'Welcome to the about page.'
+				"This is a weather application build on React. I have Built this for The Complete React Web app Developer Course."
+			),
+			React.createElement(
+				"p",
+				null,
+				"Here are some of the tools I used:"
+			),
+			React.createElement(
+				"ul",
+				null,
+				React.createElement(
+					"li",
+					null,
+					React.createElement(
+						"a",
+						{ href: "https://facebook.github.io/react" },
+						" React "
+					)
+				)
 			)
 		);
 	};
